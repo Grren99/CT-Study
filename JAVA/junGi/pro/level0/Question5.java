6ab1a93bd141ec1a7b78ccc5b9e6adf12cac0506
Grren99 <jungisong369@gmail.com>
2023-10-16 13:49:03 +0900

    -add Question5 create

---
+++ JAVA/junGi/pro/level0/Question5.java
@@ -0,0 +1,18 @@
+package JAVA.junGi.pro.level0;
+
+public class Question5 {
+
+    // 접미사인지 확인하기
+    // endWith함수 이용
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        for(int i = 0; i < my_string.length(); i ++){
+            if(my_string.endsWith(is_suffix)){
+                answer = 1;
+            }else{
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
