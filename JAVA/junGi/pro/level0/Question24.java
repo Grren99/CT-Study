01fb323d0838236b8fa52a2effd1dda4bc512c54
sgreen99 <sgreen99@naver.com>
2023-10-21 15:48:41 +0900

    -add Question24 create

---
+++ JAVA/junGi/pro/level0/Question24.java
@@ -0,0 +1,32 @@
+package JAVA.junGi.pro.level0;
+
+public class Question24 {
+
+        // 그림 확대
+        public String[] solution(String[] picture, int k) {
+            int rows = picture.length;
+            int cols = picture[0].length();
+
+            int newRows = rows * k;
+            int newCols = cols * k;
+
+            String[] newPicture = new String[newRows];
+
+            for (int i = 0; i < newRows; i++) {
+                StringBuilder sb = new StringBuilder();
+                int row = i / k;
+
+                for (int j = 0; j < newCols; j++) {
+                    int col = j / k;
+                    char pixel = picture[row].charAt(col);
+                    sb.append(pixel);
+                }
+
+                newPicture[i] = sb.toString();
+            }
+
+            return newPicture;
+
+    }
+
+}
