7e9ce0ff0c8de58504761e3d2f3708c262d68ca4
sgreen99 <sgreen99@naver.com>
2023-10-29 13:43:40 +0900

    -add Question29 create

---
+++ JAVA/junGi/pro/level0/Question29.java
@@ -0,0 +1,19 @@
+package JAVA.junGi.pro.level0;
+
+public class Question29 {
+
+        public int solution(int chicken) {
+            int answer = 0;
+
+            while (chicken >= 10) {
+                int newChicken = chicken / 10;
+                int restChicken = chicken % 10;
+                chicken = newChicken + restChicken;
+
+                answer += newChicken;
+            }
+
+            return answer;
+    }
+
+}
