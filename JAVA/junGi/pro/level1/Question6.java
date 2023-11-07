a9e9aed0b7c2ca1609f7ac41c9bf9490d29efa63
Grren99 <jungisong369@gmail.com>
2023-11-07 10:11:20 +0900

    -add level1 Question6 create

--- JAVA/junGi/pro/level1/Question6.java
+++ JAVA/junGi/pro/level1/Question6.java
@@ -1,4 +1,17 @@
 package JAVA.junGi.pro.level1;
 
 public class Question6 {
+    // 핸드폰 번호 가리기
+    // repeat 함수 사용
+    public String solution(String phone_number) {
+        int length = phone_number.length();
+        String answer = "";
+
+        String stars = "*".repeat(length - 4);
+        String lastFour = phone_number.substring(length - 4);
+        answer = stars + lastFour;
+
+        return answer;
+    }
+
 }
