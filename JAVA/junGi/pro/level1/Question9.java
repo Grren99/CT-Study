b8c5d7bd1a42796e7db170e2f102ddaa40463013
Grren99 <jungisong369@gmail.com>
2023-11-08 09:54:19 +0900

    -add level1 Question9 create

---
+++ JAVA/junGi/pro/level1/Question9.java
@@ -0,0 +1,29 @@
+package JAVA.junGi.pro.level1;
+
+public class Question9 {
+        // 시저 암호
+        public String solution(String s, int n) {
+            String answer = "";
+
+            for(int i=0; i<s.length(); i++){
+                char ch = s.charAt(i);
+                if(ch == ' '){
+                    answer+=ch;
+                }
+                if(ch >= 'a' && ch <= 'z'){
+                    if(ch+n > 'z'){
+                        answer += (char)(ch - 26 + n);
+                    }else{
+                        answer += (char)(ch + n);
+                    }
+                }else if(ch >= 'A' && ch <='Z'){
+                    if(ch+n > 'Z'){
+                        answer += (char)(ch - 26 + n);
+                    }else{
+                        answer += (char)(ch + n);
+                    }
+                }
+            }
+            return answer;
+    }
+}
