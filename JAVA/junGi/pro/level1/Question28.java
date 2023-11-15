40ecbc3befeb68a4e6cb2ba796970bad710ac258
Grren99 <jungisong369@gmail.com>
2023-11-15 12:52:22 +0900

    -add level1 Question27 ~ 28 create

---
+++ JAVA/junGi/pro/level1/Question28.java
@@ -0,0 +1,24 @@
+package JAVA.junGi.pro.level1;
+
+public class Question28 {
+        // 카드 뭉치
+        public String solution(String[] cards1, String[] cards2, String[] goal) {
+            int idx1 = 0; // cards1 배열의 인덱스
+            int idx2 = 0; // cards2 배열의 인덱스
+
+            // goal 배열 순회
+            for (String currentStr : goal) {
+                // currentStr이 cards1[idx1] 또는 cards2[idx2]에 포함하는지 확인
+                if (currentStr.equals(cards1[idx1])) {
+                    idx1++; // cards1[idx1]에 포함하는 경우 idx1 증가
+                } else if (currentStr.equals(cards2[idx2])) {
+                    idx2++; // cards2[idx2]에 포함하는 경우 idx2 증가
+                } else {
+                    return "No"; // 둘 다 포함하지 않는 경우
+                }
+            }
+
+            return "Yes";
+        }
+
+}
