64cd794752a2a3da190051df937d322e91cfc439
Grren99 <jungisong369@gmail.com>
2023-11-20 14:37:52 +0900

    -add baekjoon 5file create

---
+++ JAVA/junGi/beakjoon/bronze/N11718.java
@@ -0,0 +1,14 @@
+package JAVA.junGi.beakjoon.bronze;
+
+import java.util.Scanner;
+
+public class N11718 {
+    // 입력한 그대로 출력하기
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // sc 입력된 값이 있다면 계속 출력
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
